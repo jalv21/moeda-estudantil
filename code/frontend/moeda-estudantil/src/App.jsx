b1adfcbbba07e5bb0436617815ec83a14d1a68b1
Jalv <araujo.ja.rodrigues@gmail.com>
2026-04-20 23:13:50 -0300

    feat/adicionando ícone de moeda na página temporária

--- code/frontend/moeda-estudantil/src/App.jsx
+++ code/frontend/moeda-estudantil/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import './assets/coin-icon.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-tr from-purple-600 to-green-600">
-      <h1 className="text-6xl font-bold text-white">BrainCoin</h1>
+      <div className="flex items-center gap-2">
+        <img src="./src/assets/coin-icon.png" alt="Coin Icon" className="w-25 h-25" />
+        <h1 className="text-6xl font-bold text-white">BrainCoins</h1>
+      </div>
       <p className="text-lg text-white font-light">Sistema de moeda estudantil</p>
     </div>  
   )
