e9fe85d5a31c4103b9b22cc8423bc8db2a096411
Jalv <araujo.ja.rodrigues@gmail.com>
2026-04-22 08:30:32 -0300

    fix/frontend/ajustando cores do ícone de moeda

--- code/frontend/moeda-estudantil/src/App.jsx
+++ code/frontend/moeda-estudantil/src/App.jsx
@@ -3,15 +3,14 @@ import './App.css'
 import './assets/coin-icon.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-tr from-purple-600 to-green-600">
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-1">
         <img src="./src/assets/coin-icon.png" alt="Coin Icon" className="w-25 h-25" />
         <h1 className="text-6xl font-bold text-white">BrainCoins</h1>
       </div>
-      <p className="text-lg text-white font-light">Sistema de moeda estudantil</p>
+      <p className="text-lg text-white font-light">Sistema de Moeda Estudantil</p>
     </div>  
   )
 }
