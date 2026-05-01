4cabe66074f0e775ded10f6de682cd5ecc7439d1
Jalv <araujo.ja.rodrigues@gmail.com>
2026-05-01 12:35:55 -0300

    fix/atualizando dependências vulneráveis

--- code/backend/moeda/src/main/java/com/lab3/moeda/MoedaApplication.java
+++ code/backend/moeda/src/main/java/com/lab3/moeda/MoedaApplication.java
@@ -3,7 +3,7 @@ package com.lab3.moeda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.lab3.moeda"})
 public class MoedaApplication {
 
 	public static void main(String[] args) {
