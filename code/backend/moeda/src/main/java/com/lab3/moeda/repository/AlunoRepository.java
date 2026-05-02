b46d70ac0ac0743f09d45c3f45a25a3ffcdd4a7d
Jalv <araujo.ja.rodrigues@gmail.com>
2026-05-01 22:31:43 -0300

    feat/add interface AlunoRepository

---
+++ code/backend/moeda/src/main/java/com/lab3/moeda/repository/AlunoRepository.java
@@ -0,0 +1,10 @@
+package com.lab3.moeda.repository;
+
+import com.lab3.moeda.model.AlunoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<AlunoEntity, Integer> {
+
+}
--- code/backend/moeda/src/test/java/com/lab3/moeda/CrudTest.java
+++ code/backend/moeda/src/test/java/com/lab3/moeda/CrudTest.java
@@ -2,6 +2,7 @@ package com.lab3.moeda;
 
 import com.lab3.moeda.model.AlunoEntity;
 import com.lab3.moeda.model.UsuarioAcademicoEntity;
+import com.lab3.moeda.repository.AlunoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
@@ -10,12 +11,12 @@ import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
 @DataJpaTest
 public class CrudTest {
     @Autowired
-    private TestEntityManager entityManager;
+    private AlunoRepository alunoRepository;
 
     @Test
     void create() {
-        UsuarioAcademicoEntity alice = new AlunoEntity("Alice", "12345678910", "123456789", "Rua 1", "UFMG", "Biologia");
+        AlunoEntity alice = new AlunoEntity("Alice", "12345678910", "123456789", "Rua 1", "UFMG", "Biologia");
 
-        entityManager.persistAndFlush(alice);
+        alunoRepository.save(alice);
     }
 }
