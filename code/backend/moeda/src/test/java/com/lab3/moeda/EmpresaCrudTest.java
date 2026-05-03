7755320c0999fd32e29e949646cfa0698bccbe3d
Jalv <araujo.ja.rodrigues@gmail.com>
2026-05-03 08:19:13 -0300

    feat/usando DTO nos testes

--- code/backend/moeda/src/test/java/com/lab3/moeda/EmpresaCrudTest.java
+++ code/backend/moeda/src/test/java/com/lab3/moeda/EmpresaCrudTest.java
@@ -1,5 +1,6 @@
 package com.lab3.moeda;
 
+import com.lab3.moeda.dto.EmpresaRequestDTO;
 import com.lab3.moeda.model.EmpresaEntity;
 import com.lab3.moeda.repository.EmpresaRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,11 +13,36 @@ public class EmpresaCrudTest {
     @Autowired
     private EmpresaRepository empresaRepository;
     private EmpresaEntity cafe, papelaria, farmacia;
+    private EmpresaRequestDTO cafeRequest, papelariaRequest, farmariaRequest;
 
     @BeforeEach
     void setUp() {
         cafe = new EmpresaEntity("Café Mentor", "12345678941001", "Rua 1", "contato@cafementor.com", "123456");
         papelaria = new EmpresaEntity("Papelaria Maria", "12345678941002", "Rua 2", "contato@papelariamaria.com", "123456");
         farmacia = new EmpresaEntity("Farmácia Droga Rara", "12345678941003", "Rua 3", "contato@drogarara.com", "123456");
+
+        cafeRequest = new EmpresaRequestDTO(
+              cafe.getNome(),
+              cafe.getCnpj(),
+              cafe.getEndereco(),
+              cafe.getEmail(),
+              cafe.getSenha()
+        );
+
+        papelariaRequest = new EmpresaRequestDTO(
+                papelaria.getNome(),
+                papelaria.getCnpj(),
+                papelaria.getEndereco(),
+                papelaria.getEmail(),
+                papelaria.getSenha()
+        );
+
+        farmariaRequest = new EmpresaRequestDTO(
+                farmacia.getNome(),
+                farmacia.getCnpj(),
+                farmacia.getEndereco(),
+                farmacia.getEmail(),
+                farmacia.getSenha()
+        );
     }
 }
